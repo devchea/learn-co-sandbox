89f39fda080ff94d83e5b4a5c7d789547775cb6f
Richard Chea <rchea817@gmail.com>
2021-05-06 20:39:43 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,4 @@
+oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", "Doc", "Lemonjello", "Sleepy" ]
+oppressed_workers.each do |oppressed_worker|
+  puts "#{oppressed_worker.capitalize} wants to start a union!"
+end
